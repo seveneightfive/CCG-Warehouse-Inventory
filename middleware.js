bba686af525a24c14e12b9ebc4547291d2f930e4
seveneightfive <35607452+seveneightfive@users.noreply.github.com>
2026-09-09 22:38:07 -0500

    Update matcher to exclude additional file types

--- middleware.js
+++ middleware.js
@@ -35,5 +35,5 @@ export async function middleware(request) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\..*).*)"],
 };
